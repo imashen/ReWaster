7ab72fa6fa8ae33193573a91a88fe36497085759
Taylor Lottner <50132994+imashen@users.noreply.github.com>
2024-08-10 16:24:08 +0800

    new version

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ReWaster
+### Usage
+```
+docker pull imashen/rewaster
+docker run -d --rm -e URL=XXX[str] -e REFERER=XXX[str] -e THREAD=XXX[int] imashen/rewaster
--- ReWaster.sh
+++ ReWaster.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# 创建脚本文件
-echo "#!/bin/bash" > _ins.sh
-echo "" >> _ins.sh
-echo "REFERER=${REFERER}" >> _ins.sh
-echo "URL=${URL}" >> _ins.sh
-echo "while true" >> _ins.sh
-echo "do" >> _ins.sh
-echo "    curl -s -o /dev/null -H \"Referer: \$REFERER\" \"\$URL\"" >> _ins.sh
-echo "done" >> _ins.sh
+REFERER=${REFERER}
+URL=${URL}
+THREAD=${THREAD:-20} # 默认设置为20个线程
 
-# 设置脚本可执行权限
-chmod +x _ins.sh
+request() {
+    while true
+    do
+        curl -s -o /dev/null -H "Referer: $REFERER" "$URL"
+    done
+}
 
-# 运行多个实例
-for i in $(seq 1 ${THREAD:-20})
+for i in $(seq 1 $THREAD)
 do
-    nohup ./_ins.sh > /dev/null 2>&1 &
-    echo "线程 $i 开始工作，执行服务器重启指令reboot以关闭脚本!"
+    (request &)
 done
+
+wait
